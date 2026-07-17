5b7bc4b70de6eccf0410a4b77895207cdc6c1c0f
Subham Karmakar <rikk4335@gmail.com>
2026-07-17 05:49:05 +0530

    updated

--- frontend/playwright.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "playwright/test";
-
-export default defineConfig({
-  testDir: "./tests",
-  use: { baseURL: "http://localhost:3000" },
-  webServer: { command: "node -e \"require('fs').cpSync('.next/static', '.next/standalone/.next/static', { recursive: true, force: true })\" && node .next/standalone/server.js", url: "http://localhost:3000", reuseExistingServer: false },
-});
